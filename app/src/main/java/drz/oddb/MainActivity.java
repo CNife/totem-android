05d82ecf6ceac2f0ee332de6fb8614577a578c2b
Joooook <1046412089@qq.com>
2018-12-17 17:27:17 +0800

    memory change

--- app/src/main/java/drz/oddb/MainActivity.java
+++ app/src/main/java/drz/oddb/MainActivity.java
@@ -3,11 +3,6 @@ package drz.oddb;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.io.File;
-import java.nio.ByteBuffer;
-import java.util.HashMap;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
