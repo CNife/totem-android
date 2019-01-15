6bfe4f3cf95705c462a7a4882f3f5a41eff35379
17pigfarmer <814207734@qq.com>
2019-01-15 23:23:30 +0800

    trans by dz

---
+++ app/src/main/java/drz/oddb/Transaction/SystemTable.java
@@ -0,0 +1,50 @@
+package drz.oddb.Transaction;
+
+
+import java.util.List;
+
+class TopTable{       //总表
+    List<TopTableItem> topTable;
+
+}
+
+class TopTableItem{
+    String dbname = null; //数据库名
+    int dbid = 0;       //数据库id
+    int classid = 0;    //类id
+    int tupleid = 0;    //元组id
+    int blockid = 0;    //块id
+    int offset = 0;      //元祖偏移量
+}
+
+class ClassTable{       //类表
+    List<ClassTableItem> classTable;
+    int maxid=0;
+
+}
+
+class ClassTableItem {
+    String classname = null;        //类名
+    int classid = 0;                //类id
+    int attrnum = 0;                //类属性个数
+    String attrname = null;         //属性名
+    String attrtype = null;         //属性类型
+
+    public ClassTableItem(String classname, int classid, int attrnum, String attrname, String attrtype) {
+        this.classname = classname;
+        this.classid = classid;
+        this.attrnum = attrnum;
+        this.attrname = attrname;
+        this.attrtype = attrtype;
+    }
+}
+
+class DeputyTable {     //代理类表
+    List<DeputyTableItem> deputyTable;
+
+}
+class DeputyTableItem{
+    int classid = 0;            //类id
+    int deputyid = 0;           //代理类id
+    String deputynum = null;    //代理类名
+}
--- app/src/main/java/drz/oddb/Transaction/TransAction.java
+++ app/src/main/java/drz/oddb/Transaction/TransAction.java
@@ -3,24 +3,31 @@ package drz.oddb.Transaction;
 import java.io.InputStream;
 import java.io.ByteArrayInputStream;
 import java.util.*;
+import drz.oddb.Memory.*;
 
 
 
 import drz.oddb.parse.*;
 
 public class TransAction {
-    public String query(String s){
+
+    TopTable topt = new TopTable();
+    ClassTable classt = new ClassTable();
+    DeputyTable deputyt = new DeputyTable();
+
+    public String query(String s) {
 
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(s.getBytes());
         parse p = new parse(byteArrayInputStream);
         try {
             String[] aa = p.Run();
 
-            switch (Integer.parseInt(aa[0]))
-            {
+            switch (Integer.parseInt(aa[0])) {
                 case parse.OPT_CREATE_ORIGINCLASS:
-                break;
+                    CreateOriginClass(aa);
+                    break;
                 case parse.OPT_CREATE_SELECTDEPUTY:
+
                     break;
                 case parse.OPT_DROP:
                     break;
@@ -44,4 +51,22 @@ public class TransAction {
         return s;
 
     }
+
+
+    private void CreateOriginClass(String[] p) {
+        String classname = p[1];
+        int count = Integer.parseInt(p[2]);
+        classt.maxid++;
+        int classid = classt.maxid;
+        for (int i = 0;i<count;i++){
+            classt.classTable.add(new ClassTableItem(classname,classid,count,p[2*i+3],p[2*i+4]));
+        }
+    }
+
+    private void CreateSelectDeputy(String[] p){
+
+    }
+
+
+
 }
