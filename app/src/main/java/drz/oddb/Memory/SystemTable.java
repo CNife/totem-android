4490cd826f5f2fa35f3b5242728e6ce1cff82b7a
EternalLAD <1447421864@qq.com>
2019-01-15 19:33:41 +0800

    系统表已实现，未实现函数

--- app/src/main/java/drz/oddb/Memory/SystemTable.java
+++ app/src/main/java/drz/oddb/Memory/SystemTable.java
@@ -1,7 +1,6 @@
 package drz.oddb.Memory;
 
 
-import java.io.File;
 import java.util.List;
 
 class TopTable{       //数据库id，表id关系表
@@ -12,37 +11,43 @@ class TopTable{       //数据库id，表id关系表
         int tupleid = 0;    //元组id
         int blockid = 0;    //块id
     }
-
-
-    //public boolean write(){
-
-    //}
-
 }
 
 class ClassTable{       //类id，表id关系表
-    int classid = 0;                //类id
-    String classname = null;        //类名
-    int tableid = 0;                //表id
-    String tablename = null;        //表名
-    int attrnum = 0;                //表属性个数
+    List<ClassTableItem> classTable;
+    class ClassTableItem{
+        int classid = 0;                //类id
+        String classname = null;        //类名
+        int tableid = 0;                //表id
+        String tablename = null;        //表名
+        int attrnum = 0;                //表属性个数
+    }
 }
 
 class DbTable {         //数据库id，数据库名
-    int dbid = 0;            //数据库id
-    String dbname = null;    //表名
+    List<DbTableItem> dbTable;
+    class DbTableItem{
+        int dbid = 0;            //数据库id
+        String dbname = null;    //表名
+    }
 }
 
 
 class AttrTable {       //属性表
-    int chassid = 0;         //类id
-    int tableid = 0;         //表id
-    String attrname = null;  //属性名
-    String type = null;      //属性类型
+    List<AttrTableItem> attrTable;
+    class AttrTableItem{
+        int chassid = 0;         //类id
+        int tableid = 0;         //表id
+        String attrname = null;  //属性名
+        String type = null;      //属性类型
+    }
 }
 
 class DeputyTable {     //代理类表
-    int classid = 0;            //类id
-    int deputyid = 0;           //代理类id
-    String deputynum = null;    //代理类名
+    List<DeputyTableItem> deputyTable;
+    class DeputyTableItem{
+        int classid = 0;            //类id
+        int deputyid = 0;           //代理类id
+        String deputynum = null;    //代理类名
+    }
 }
