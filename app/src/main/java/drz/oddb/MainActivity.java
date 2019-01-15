8ca173f4ac0613c72a64865a4fa88484045982b2
AmenesiaR <274104022@qq.com>
2019-01-15 14:48:05 +0800

    test qhrnb

--- app/src/main/java/drz/oddb/MainActivity.java
+++ app/src/main/java/drz/oddb/MainActivity.java
@@ -1,13 +1,71 @@
 package drz.oddb;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    //查询输入框
+    private EditText editText;
+
+    //BGM
+    //private Intent intent = new Intent("com.angel.Android.MUSIC");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = new Intent(MainActivity.this,MusicServer.class);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //播放BGM
+        startService(intent);
+
+        //查询按钮
+        Button button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.edit_text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //在此处添加button按钮逻辑
+            }
+        });
     }
+    protected void onStop(){
+        Intent intent = new Intent(MainActivity.this,MusicServer.class);
+        stopService(intent);
+        super.onStop();
+    }
+
+    //对输入框输入的内容进行检测，若格式不正确，则弹出对话框提示
+    /*根据需求添加，暂时不加入*/
+    /*@Override
+    public void onCheck(){
+        if(
+        //格式错误
+         ) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+            dialog.setTitle("Attention");
+            dialog.setMessage("Please enter the correct query content!");
+            dialog.setCancelable(false);
+            dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+            dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+            dialog.show();
+        }
+    }
+    */
+
 }
