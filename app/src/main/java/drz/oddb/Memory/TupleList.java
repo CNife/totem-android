beeca6fb861d84cfafaf26b3187703fd91fa2741
hxhk <277970751@qq.com>
2019-01-16 18:05:25 +0800

    hx

--- app/src/main/java/drz/oddb/Memory/TupleList.java
+++ app/src/main/java/drz/oddb/Memory/TupleList.java
@@ -2,7 +2,7 @@ package drz.oddb.Memory;
 import java.util.*;
 public class TupleList {
     public List<Tuple> tuplelist;
-    int tuplenum = 0;
+    public int tuplenum = 0;
     public void addTuple(Tuple tuple){
         this.tuplelist.add(tuple);
         tuplenum++;
---
+++ app/src/main/java/drz/oddb/Transaction/SystemTable.java
@@ -0,0 +1,66 @@
+package drz.oddb.Transaction;
+
+
+import java.util.List;
+
+class TopTable{       //总表
+    List<TopTableItem> topTable;
+    public int maxTupleId = 0;
+}
+
+class TopTableItem{
+    String dbname = null; //数据库名
+    int dbid = 0;       //数据库id
+    int classid = 0;    //类id
+    int tupleid = 0;    //元组id
+    int blockid = 0;    //块id
+    int offset = 0;      //元祖偏移量
+
+    public TopTableItem(String dbname, int dbid, int classid, int tupleid, int blockid, int offset) {
+        this.dbname = dbname;
+        this.dbid = dbid;
+        this.classid = classid;
+        this.tupleid = tupleid;
+        this.blockid = blockid;
+        this.offset = offset;
+    }
+}
+
+class ClassTable{       //类表
+    List<ClassTableItem> classTable;
+    int maxid=0;
+
+}
+
+class ClassTableItem {
+    String classname = null;        //类名
+    int classid = 0;                //类id
+    int attrnum = 0;                //类属性个数
+    int    attrid = 0;
+    String attrname = null;         //属性名
+    String attrtype = null;         //属性类型
+
+    public ClassTableItem(String classname, int classid, int attrnum,int attrid, String attrname, String attrtype) {
+        this.classname = classname;
+        this.classid = classid;
+        this.attrnum = attrnum;
+        this.attrname = attrname;
+        this.attrtype = attrtype;
+        this.attrid = attrid;
+    }
+}
+
+class DeputyTable {     //代理类表
+    List<DeputyTableItem> deputyTable;
+}
+class DeputyTableItem{
+    int classid = 0;            //类id
+    int deputyid = 0;           //代理类id
+    String deputyname = null;    //代理类名
+
+    public DeputyTableItem(int classid, int deputyid, String deputyname) {
+        this.classid = classid;
+        this.deputyid = deputyid;
+        this.deputyname = deputyname;
+    }
+}
