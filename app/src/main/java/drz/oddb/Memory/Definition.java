df642633a3bc4f4c15a26411d6a10d5cf931ca60
EternalLAD <1447421864@qq.com>
2019-01-18 19:46:31 +0800

    Merge branch 'master' of C:\Users\szl\Desktop\Oddb1 with conflicts.

--- app/src/main/java/drz/oddb/Memory/Definition.java
+++ app/src/main/java/drz/oddb/Memory/Definition.java
@@ -5,14 +5,6 @@ import java.util.List;
 //磁盘
 class PageHeaderData{
     int startFreespace;					//空闲空间开始位置
-    int endFreespace;					//空闲空间结束位置
-    int tupleNum;						//元组数
-    boolean[] flag = new  boolean[1024];	//位图
-}
-
-class header{
-    int recordLen;
-    boolean[] flag = new  boolean[50];		//位图
 }
 
 //缓冲区
@@ -22,20 +14,30 @@ class buffPointer {
     int buf_id;		//缓冲区索引号
 }
 
-//内存
-class HeapTupleHeader{
-    int attrNum;						//属性个数
-    int[] attrType = new int[10]; 			//属性类型（0为String, 1为int. 默认为0）
-    String[] attrName = new String [10];	//属性名
-}
-
 class ItemPointerData{
+    Integer blockNum;
     int offset;			//块内偏移
 }
 
-class HeapTupleData{
+
+
+/**************************/
+/*class header{
+    int recordLen;
+    boolean[] flag = new  boolean[50];		//位图
+}*/
+
+/*class HeapTupleHeader{
+    int attrNum;						//属性个数
+    int[] attrType = new int[10]; 			//属性类型（0为String, 1为int. 默认为0）
+    String[] attrName = new String [10];	//属性名
+}*/
+
+
+/*class HeapTupleData{
     ItemPointerData	t_self;	//记录块号及偏移
     HeapTupleHeader	t_data;	//表头信息
     boolean[] flag = new  boolean[50];		//记录对应属性是否为空（0为空，1不为空）
     List <String> value;	//属性值
-}
+}*/
+/**************************/
