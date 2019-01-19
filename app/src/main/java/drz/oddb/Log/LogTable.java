911a78990146d83880e0287bebf80350b8ad25b5
Joooook <1046412089@qq.com>
2019-01-19 14:50:34 +0800

    for log

--- app/src/main/java/drz/oddb/Log/LogTable.java
+++ app/src/main/java/drz/oddb/Log/LogTable.java
@@ -5,6 +5,6 @@ import java.util.List;
 
 public class LogTable {
     public int check=0;
-    public int logNo=0;
+    public int logID=0;
     public List<LogTableItem> logTable=new ArrayList<>();
 }
